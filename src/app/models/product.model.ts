a29116cb069332face399c91333c6ebbf574d593
Erik Cunha <erik.cunha@venturus.org.br>
2019-10-04 12:29:16 -0300

    first commit

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,8 @@
+export class ProductModel {
+  public id: string;
+  public title: string;
+  public category: string;
+  public description: string;
+  public price: number;
+  public images: string[];
+}
